7fc653ed4fd1dc6228b5d600b50e639391b4a9d1
loco <loco@tokyo.lan>
2019-05-25 18:34:17 -0400

    Electron first app

---
+++ electron/helloWorld/README.txt
@@ -0,0 +1,5 @@
+- Install Electron by typing in the helloworld directory:
+$ npm install --save-dev electron
+
+- Start the app by typing:
+$ npm start
---
+++ electron/helloWorld/main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  // Create the browser window.
+  let win = new BrowserWindow({
+    width: 600,
+    height: 400,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  //win.webContents.openDevTools()
+  // and load the index.html of the app.
+  win.loadFile('index.html')
+}
+
+app.on('ready', createWindow)
