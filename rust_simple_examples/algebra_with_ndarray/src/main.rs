795d895744a48a6e2aa9f85ec821ec52d5f1b5e8
xoffio <marquessricardo@gmail.com>
2022-11-18 00:43:35 -0500

    algebra with ndarray and rust - updated

--- rust_simple_examples/algebra_with_ndarray/src/main.rs
+++ rust_simple_examples/algebra_with_ndarray/src/main.rs
@@ -4,5 +4,10 @@ fn main() {
     types::matrix::print_matrix_ones();
     types::matrix::print_matrix_zeros();
     types::matrix::print_matrix_rand();
+    types::matrix::print_matrix_identity();
     types::vector::print_vector();
+
+    types::matrix::print_matrix_addition();
+    types::matrix::print_matrix_scalar_mul_div();
+    types::matrix::print_matrix_dot_product();
 }
--- rust_simple_examples/algebra_with_ndarray/src/types/matrix.rs
+++ rust_simple_examples/algebra_with_ndarray/src/types/matrix.rs
@@ -3,7 +3,7 @@ use ndarray_rand::RandomExt;
 use ndarray_rand::rand_distr::Uniform;
 
 pub fn print_matrix(){
-    // Create a 2d matrix. (2x3)
+    // Create a 2d matrix. (3x2)
     let _a = arr2(&[[1, 2],
                                                      [3, 4],
                                                      [5, 6]]);
@@ -11,8 +11,17 @@ pub fn print_matrix(){
     // Another way to create it.
     let a = arr2(&[[1, 2], [3, 4], [5, 6]]);
 
+    // Another way to create a 1d, 2d or 3d matrix.
+    let b = array![[1, 2], [3, 4], [5, 6]];
+
     println!("--------");
     println!("{}", a);
+    println!("{:?}", a.dim());
+    println!("{:?}", a.len());
+    println!("--");
+    println!("{}", b);
+    println!("{:?}", b.dim());
+    println!("{:?}", b.len());
     println!("--------\n");
 }
 
@@ -55,5 +64,47 @@ pub fn print_matrix_rand(){
 
     println!("--------");
     println!("{:1.3}", r);
+    println!("--------\n");
+}
+
+pub fn print_matrix_identity(){
+    let i_matrix = Array::<f64, _>::eye(5);
+    //let i_matrix_a = Array2::<f64>::eye(5);
+
+    println!("--------");
+    println!("{}", i_matrix);
+    println!("--------\n");
+}
+
+pub fn print_matrix_addition(){
+    let ma = array![[1, 2], [3, 4], [50, 60]];
+
+    // Create a 3x2 matrix full of 2
+    let mb = Array::from_elem((3, 2), 2);
+
+    println!("--------");
+    println!("{}", ma+mb);
+    println!("--------\n");
+}
+
+pub fn print_matrix_scalar_mul_div(){
+    let scalar = 2;
+    let ma = array![[10, 2], [30, 4], [50, 6]];
+
+    println!("--------");
+    println!("{}", scalar*&ma);
+    println!("{}", ma/scalar);
+    println!("--------\n");
+}
+
+pub fn print_matrix_dot_product(){
+    let v = arr1(&[1, 2]);
+    let ma = array![[10, 2], [30, 4], [50, 6]];
+
+    println!("--------");
+    println!("{}", &ma.dot(&v));
+    
+    // the next is not valid and it will panic
+    //println!("{}", &v.dot(&ma));
     println!("--------\n");
 }
