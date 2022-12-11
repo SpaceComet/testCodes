0141b793ea84dcc747fb61d623cfbc7b53db705a
xoffio <marquessricardo@gmail.com>
2022-12-10 22:31:41 -0500

    code updated to execute the test multiple times

--- rust_simple_examples/ndarray_benchmark/src/main.rs
+++ rust_simple_examples/ndarray_benchmark/src/main.rs
@@ -4,7 +4,7 @@ use std::time::Instant;
 
 mod reader;
 
-fn scalar_calculation(x: Vec<f64>){
+fn scalar_calculation(x: &Vec<f64>) -> (u128, f64) {
     let theta = [1.0, 2.0];
     let now = Instant::now();
     let mut y = 0.0;
@@ -13,38 +13,61 @@ fn scalar_calculation(x: Vec<f64>){
         y += theta[0] + theta[1] * x_i;
     }
 
-    let elapsed = now.elapsed();
-    println!("--- Scalar ---");
-    println!("Result: {:.2?}", y);
-    println!("Elapsed: {:.2?}\n", elapsed);
+    let elapsed = now.elapsed().as_micros();
+
+    (elapsed, y)
 }
 
-fn scalar_rayon_calculation(x: Vec<f64>){
+fn scalar_rayon_calculation(x: &Vec<f64>) -> (u128, f64) {
     let theta = [1.0, 2.0];
     let now = Instant::now();
     let y: f64 = x.par_iter().map(|xi| theta[0] + theta[1] * xi).sum();
-    let elapsed = now.elapsed();
-    
-    println!("--- Scalar Rayon ---");
-    println!("Result: {:.2?}", y);
-    println!("Elapsed: {:.2?}\n", elapsed);
+    let elapsed = now.elapsed().as_micros();
+
+    (elapsed, y)
 }
 
-fn vectorized_calculation(x: Array1<f64>){
+fn vectorized_calculation(x: &Array1<f64>) -> (u128, f64) {
     let theta = arr1(&[1.0, 2.0]);
-    let x = stack!(Axis(1), Array1::<f64>::ones(x.len()), x);
+    let x = stack!(Axis(1), Array1::<f64>::ones(x.len()), x.clone());
+    
     let now = Instant::now();
     let y = x.dot(&theta).sum(); 
-    let elapsed = now.elapsed();
+    let elapsed = now.elapsed().as_micros();
 
-    println!("--- Vectorized ndarray ---");
-    println!("Result: {:.2?}", y);
-    println!("Elapsed: {:.2?}\n", elapsed);
+    (elapsed, y)
 }
 fn main() {
-    let (arr, arr_nd) = reader::read_file("../../shared_data/kc_house_data_sm_mod.csv");
+    let (arr, arr_nd) = reader::read_file("../../shared_data/kc_house_data.csv");
     
-    scalar_calculation(arr.clone());
-    scalar_rayon_calculation(arr);
-    vectorized_calculation(arr_nd);
+    let n_tests = 100000.0;
+    let mut sc_total: u128 = 0;
+    let mut src_total: u128 = 0;
+    let mut vc_total: u128 = 0;
+
+    println!("Running {} tests with an array of {} elements...", n_tests, arr.len());
+
+    for _n in 0..(n_tests as i64) {
+        let sc_result = scalar_calculation(&arr);
+        let src_result = scalar_rayon_calculation(&arr);
+        let vc_result = vectorized_calculation(&arr_nd);
+
+        sc_total += sc_result.0;
+        src_total += src_result.0;
+        vc_total += vc_result.0;
+        
+        assert_eq!(23345871629.0, sc_result.1);
+        assert_eq!(23345871629.0, src_result.1);
+        assert_eq!(23345871629.0, vc_result.1);
+    }
+
+    println!("--- Scalar ---");
+    println!("Elapsed (μs): {}\n", (sc_total as f64)/n_tests);
+    
+    println!("--- Scalar Rayon ---");
+    println!("Elapsed (μs): {}\n", (src_total as f64)/n_tests);
+
+    println!("--- Vectorized ndarray ---");
+    println!("Elapsed (μs): {:.2?}\n", (vc_total as f64)/n_tests);
+
 }
