1a5c908cb879952e217e6021b8b64e0758a39eac
xoffio <marquessricardo@gmail.com>
2023-07-22 15:29:28 -0400

    simple axum code added

---
+++ rust_simple_examples/axum_simple/src/main.rs
@@ -0,0 +1,22 @@
+use axum::{response::Json, routing::get, Router};
+use serde_json::{json, Value};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/plain_text", get(plain_text))
+        .route("/json", get(json));
+
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn plain_text() -> &'static str {
+    "test"
+}
+
+async fn json() -> Json<Value> {
+    Json(json!({ "data": 42 }))
+}
