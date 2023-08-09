bd5726a7110016e0bd9dd48886634f2d6138ad8e
xoffio <marquessricardo@gmail.com>
2023-08-08 22:57:08 -0400

    updated axum example

--- rust_simple_examples/axum_simple/src/main.rs
+++ rust_simple_examples/axum_simple/src/main.rs
@@ -1,7 +1,11 @@
+//https://docs.rs/axum/latest/axum/
+
 use std::collections::HashMap;
 
 use axum::extract::{Path, Query};
+use axum::http::StatusCode;
 use axum::{response::Json, routing::get, Router};
+use serde::Deserialize;
 use serde_json::{json, Value};
 
 #[tokio::main]
@@ -11,7 +15,8 @@ async fn main() {
         .route("/json", get(json))
         .route("/say_hello_to", get(say_hello_to))
         .route("/:version/*path", get(extract_path))
-        .route("/test/test/test", get(plain_text));
+        .route("/test/test/test", get(plain_text))
+        .route("/fails_if_two", get(fails_if_two));
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
@@ -31,6 +36,17 @@ async fn json() -> Json<Value> {
     Json(json!({ "data": 42 }))
 }
 
+// Error response
+async fn fails_if_two(
+    Query(payload): Query<NumQuery>,
+) -> Result<(StatusCode, String), (StatusCode, String)> {
+    if payload.n == 2 {
+        Err((StatusCode::INTERNAL_SERVER_ERROR, "Error 500!".to_string()))
+    } else {
+        Ok((StatusCode::ACCEPTED, "Ok!".to_string()))
+    }
+}
+
 // ---- Extractors ----
 
 // Plain text response
@@ -45,4 +61,10 @@ async fn say_hello_to(Query(params): Query<HashMap<String, String>>) -> String {
 // Path extractor. Plain text response.
 async fn extract_path(Path((version, api_path)): Path<(String, String)>) -> String {
     format!("version: {}, \npath: {}", version, api_path)
+}
+
+// Queries structs
+#[derive(Deserialize, PartialEq)]
+struct NumQuery {
+    n: i32,
 }
