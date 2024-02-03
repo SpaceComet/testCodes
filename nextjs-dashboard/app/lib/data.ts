367e3745b9c23ab8bc9cb3a64490d58f9fd60d37
xoffio <marquessricardo@gmail.com>
2024-02-02 23:31:09 -0500

    chapter 7 - fetch invoices

--- nextjs-dashboard/app/lib/data.ts
+++ nextjs-dashboard/app/lib/data.ts
@@ -36,12 +36,13 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
-    const data = await sql<LatestInvoiceRaw>`
+    // const data = await sql<LatestInvoiceRaw>`
+    const data = await conn.query<LatestInvoiceRaw>(`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
-      LIMIT 5`;
+      LIMIT 5`);
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
